0f20d466c4a9382a9cb4ee9957bfa86dc2fa441c
rmc8 <49588673+rmc8@users.noreply.github.com>
2021-01-24 08:50:59 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import PySimpleGUI as sg
+from pyperclip import copy
+
+
+def rm(txt):
+    txt = txt.replace("... ", "", 1)
+    txt = txt.replace(">>> ", "", 1)
+    return txt
+
+
+def stripper(txt):
+    lines = txt.splitlines()
+    lines = [rm(l) for l in lines]
+    return "\n".join(lines)
+
+
+def gui():
+    sg.theme("Black")
+    layout = [
+        [sg.T("Please enter the code listed in the interpreter.")],
+        [sg.Multiline(size=(48, 8), key="code")],
+        [sg.Submit("Copy")],
+    ]
+
+    window = sg.Window("Code Extractors", grab_anywhere=False).Layout(layout)
+    while True:
+        event, val = window.Read()
+        if event in (None, ):
+            break
+        elif event == "Copy":
+            code = stripper(val["code"])
+            copy(code)
+            window["code"].update("")
+    window.Close()
+
+
+def main():
+    gui()
+
+
+if __name__ == "__main__":
+    main()
